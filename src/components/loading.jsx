b38f03d5dfea560b9b7fce1702f49eae1df2cfc3
Manish <mkthakur2301@gmail.com>
2025-06-18 00:40:47 +0530

    add pre loader and fixing hero elements

---
+++ src/components/loading.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function loading() {
+
+
+    return (
+        <>
+            <div className='wrapper-container h-screen w-full flex justify-center items-center'>
+                <div class="wrapper">
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="shadow"></div>
+                    <div class="shadow"></div>
+                    <div class="shadow"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default loading
