0ab8c54c11e7ba2b403bccaa0e4506cc839a13b7
Manish <mkthakur2301@gmail.com>
2025-06-25 17:43:24 +0530

    add footer section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
 
         {/* Logo */}
         <div className="logo h-full w-14 flex justify-center items-center">
-          <img className="cursor-pointer" src="Logo.png" alt="Logo" />
+          <img className="cursor-pointer" src="logo_1.png" alt="Logo" />
         </div>
 
         {/* Menu */}
@@ -24,18 +24,14 @@ function Navbar() {
           <div className="cursor-pointer hover:scale-110">CONTACT</div>
           <div className="group cursor-pointer hover:scale-110">
             ABOUT
-            <i className="ri-arrow-right-up-line inline-block transition-transform duration-300 group-hover:rotate-90"></i>
+            <i className="ri-arrow-right-up-line inline-block transition-transform duration-300 group-hover:rotate-90 group-hover:text-green-400"></i>
           </div>
         </div>
 
         {/* Search */}
-        <div className="p-2 flex items-center text-sm border border-black rounded-4xl">
-          <input
-            type="text"
-            placeholder="Search"
-            className="border-none focus:outline-none"
-          />
-          <i className="ri-search-line text-sm cursor-pointer ml-2"></i>
+        <div className=" group p-2 flex items-center gap-2 hover:bg-gray-200 rounded-md transition-all duration-300">
+          <div className="cursor-pointer">PAYMENT</div>
+          <i class="ri-qr-code-line group-hover:rotate-180 group-hover:scale-150 group-hover:text-blue-600 transform transition duration-300 ease-in-out"></i>
         </div>
 
       </div>
