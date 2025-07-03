4a95579fd928104336f4b0a14349ad1ad9de090d
Manish <mkthakur2301@gmail.com>
2025-07-03 11:37:38 +0530

    attach backend url in contat

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -23,7 +23,7 @@ const ContactUs = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://ghoomoyatri-backend.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
